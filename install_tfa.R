3b88f1e2c19d38484ba31e893dbe916e85f197b6
relund <junk@relund.dk>
2020-09-15 11:09:34 +0200

    Updated packages

--- install_tfa.R
+++ install_tfa.R
@@ -1,1 +1,2 @@
-devtools::install("tfa-package", quick = T, force = T)
+devtools::install("tfa-package", quick = T, force = T)   # local
+remotes::install_github("bss-osca/tfa/tfa-package")      # github
