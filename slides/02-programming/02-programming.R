5a8996fa2415c72645e4aa9c365531d96e174fce
Lars Relund Nielsen <junk@relund.dk>
2020-10-29 15:13:50 +0100

    Added solution

--- slides/02-programming/02-programming.R
+++ slides/02-programming/02-programming.R
@@ -1,4 +1,4 @@
-
+# !diagnostics off
 
 ## Slide 3 ----
 x <- c(1, 2, 45, NA, -1)
@@ -10,43 +10,38 @@ all(x > -100)
 
 ## Slide 11 ----
 library(tidyverse)
-
-## Hint
-# dice <- 1:6
-# card_suit <- c("clubs", "diamonds", "hearts", "spades")
-# card_number <- c("ace", 2:10, "jack", "queen", "king")
-# ite <- expand_grid(Dice = dice,
-#                    Card_suit = ___,
-#                    ___)
-# for (r in 1:nrow(ite)) { # iterate over rows
-#    cat("Dice = ", ___, " and card = ", ___, " (", ___, ").\n", sep="")
-# }
-
-
+dice <- 1:6
+card_suit <- c("clubs", "diamonds", "hearts", "spades")
+card_number <- c("ace", 2:10, "jack", "queen", "king")
+ite <- expand_grid(Dice = dice,
+                   Card_suit = card_suit,
+                   Card_number = card_number)
+for (r in 1:nrow(ite)) { # iterate over rows
+   cat("Dice = ", ite$Dice[r], " and card = ", ite$Card_number[r], " (",ite$Card_suit[r], ").\n", sep="")
+}
 
 
 ## Slide 17 ----
-
-## Hint
-# get_combinations <- function(dice, card) {
-#   ite <- expand_grid(___)
-#   found <- FALSE
-#   for (i in 1:nrow(ite)) {
-#     ___
-#   }
-#   return(found)
-# }
-# get_combinations(dice = c(2,3), card = c("2-spade", "ace-dimond"))
-# get_combinations(dice = c(3,1), card = c("10-heart", "king-dimond"))
-
+get_combinations <- function(dice, card) {
+  ite <- expand_grid(Dice = dice, Card = card)
+  found <- FALSE
+  for (i in 1:nrow(ite)) {
+     cat("Dice = ", ite$Dice[r], " and card = ", ite$Card[r], ".\n", sep="")
+  }
+  return(found)
+}
+get_combinations(dice = c(2,3), card = c("2-spade", "ace-dimond"))
+get_combinations(dice = c(3,1), card = c("10-heart", "king-dimond"))
 
 
 ## Slide 18
-
-# get_PV <- function(FV, r = 0.1, n, round = FALSE) {
-#  ---
-# }
-# get_PV(100, n = 7)
-# get_PV(100, n = 7, round = TRUE)
-# get_PV(100, n = 7, r = 0.5)
+get_PV <- function(FV, r = 0.1, n, round = FALSE) {
+   PV = FV / (1+r)^n
+   if (round) return(round(PV, 2))
+   return(PV)
+}
+get_PV(100, n = 7)
+get_PV(100, n = 7, round = TRUE)
+get_PV(100, n = 7, r = 0.5)
+warnings()
 
