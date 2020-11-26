12563af93e17c430ee9861f9d6aa0e8b52df2873
Lars Relund Nielsen <junk@relund.dk>
2020-11-26 20:58:10 +0100

    Updated script

--- install_tfa.R
+++ install_tfa.R
@@ -1,4 +1,3 @@
 devtools::install("tfa-package", quick = T, force = T)   # local
 remotes::install_github("bss-osca/tfa/tfa-package", upgrade = FALSE)      # github
-
 renv::snapshot(prompt = FALSE, force = TRUE)
