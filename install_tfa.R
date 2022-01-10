8772685b8208ee9b0d5beadde0bac8eefd007264
Lars Relund Nielsen <junk@relund.dk>
2022-01-10 11:06:20 +0100

    Testing

--- install_tfa.R
+++ install_tfa.R
@@ -1,3 +1,4 @@
 devtools::install("tfa-package", quick = T, force = T)   # local
 remotes::install_github("bss-osca/tfa/tfa-package", upgrade = FALSE)      # github
 renv::snapshot(prompt = FALSE, force = TRUE)
+
