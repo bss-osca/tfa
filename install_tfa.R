cdacbef6bb947fbe354955f6811373af0b52d8e9
relund <junk@relund.dk>
2022-07-22 13:18:08 +0200

    Update packages

--- install_tfa.R
+++ install_tfa.R
@@ -2,3 +2,4 @@ devtools::install("tfa-package", quick = T, force = T)   # local
 remotes::install_github("bss-osca/tfa/tfa-package", upgrade = FALSE)      # github
 renv::snapshot(prompt = FALSE, force = TRUE)
 
+remotes::install_github("rstudio/gradethis")
