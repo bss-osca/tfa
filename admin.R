01be20396d8fccd83e477d23a728756c1945bef6
Lars Relund <junk@relund.dk>
2023-07-12 12:37:55 +0200

    Update packages

--- admin.R
+++ admin.R
@@ -1,3 +1,10 @@
+## When start course again
+renv::restore()   # restore packages
+bookdown::serve_book(dir = "book", output_dir = "_book", preview = FALSE, in_session = TRUE, quiet = FALSE)  # knit book to see if works
+renv::update()
+renv::snapshot()
+
+## Install packages
 devtools::install("tfa-package", quick = T, force = T)   # local
 remotes::install_github("bss-osca/tfa-package", upgrade = FALSE)      # github
 remotes::install_github("bss-osca/tfa-package", upgrade = TRUE, build = FALSE)      # github
@@ -6,3 +13,5 @@ renv::snapshot(prompt = FALSE, force = TRUE)
 
 remotes::install_github("rstudio/gradethis")
 remotes::install_github("lmullen/genderdata")
+remotes::install_github("mitchelloharawild/icons")
+remotes::install_github("rstudio-education/dsbox")
