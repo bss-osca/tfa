2dc9b0fb5769e114bb48b4530ef16825f8894fba
Lars Relund <junk@relund.dk>
2024-06-20 10:43:23 +0200

    Update steps

--- admin.R
+++ admin.R
@@ -5,7 +5,7 @@
 # - Make a copy of the pref sheet https://docs.google.com/spreadsheets/d/14LOGGBGnriLFOAOxXPMsmjruxlMoN2Acl1Xf2FE5nnY/edit?usp=sharing (so can clean and use the current)
 # - Delete the prefs (so the same link is valid) in sheet https://docs.google.com/spreadsheets/d/14LOGGBGnriLFOAOxXPMsmjruxlMoN2Acl1Xf2FE5nnY/edit?usp=sharing
 # - Signin to datacamp using Econ email and create a new classroom "Tools for Analytics YYYY" at https://www.datacamp.com/universities#classroom-form (valid from 1 Sep, students 150)
-# - Add todo in cal 1/9 (add assignments and change signup link in link.md line 9)
+# - Add todo to your calendar 1/9 (add assignments and change signup link in link.md line 9)
 # - At Posit Cloud update the 'Tools for Analytics' workspace (R and packages)
 
 
@@ -24,10 +24,17 @@ bookdown::serve_book(dir = "book", output_dir = "_book", preview = FALSE, in_ses
 # Check github actions: commit, pull and push and check if GHA works
 
 
+## Update exams
+# - Copy missing exams to the exams folder
+# - Convert exams to exercises
+bookdown::serve_book(dir = "book", output_dir = "_book", preview = FALSE, in_session = TRUE, quiet = FALSE)  # knit book to see if works
+# Check github actions: commit, pull and push and check if GHA works
+
+
 
 #### Install packages (if missing) ####
 # TFA package
-devtools::install("tfa-package", quick = T, force = T)   # local
+devtools::install("../tfa-package", quick = T, force = T)   # local
 remotes::install_github("bss-osca/tfa-package", upgrade = FALSE)      # github
 remotes::install_github("bss-osca/tfa-package", upgrade = TRUE, build = FALSE)      # github
 # non CRAN packages
